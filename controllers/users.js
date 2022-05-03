faa26ffa2985811d54afd90f51437cc4e267ae02
Poli03 <99154416+Poli03@users.noreply.github.com>
2022-05-03 16:00:02 -0500

    validation of user fields has been added1

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,5 @@
 const {response, request} = require('express');
 const bcryptjs= require('bcryptjs');
-const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 
@@ -23,12 +22,6 @@ const usersPut = (req, res=response) => {
 }
 
 const usersPost = async (req, res=response) => {
-    const errors =validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json(errors);
-    }
-
-
     const {name,email, password, role}= req.body;
     const user = new User({name,email, password, role});
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const { Router }  = require('express');
 const { check }  = require('express-validator');
+
 const { usersGet, usersPost, usersDelete, usersPut, usersPatch } = require('../controllers/users');
+const { validateFields } = require('../middlewares/validate-fields');
 
 const router = Router();
 
@@ -10,6 +12,10 @@ router.put('/:id', usersPut);
 
 router.post('/',
     [check('email','Esto no es correo valido').isEmail(),
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'La contraseña debe de ser mayor a 6 caracteres').isLength({min:6}),
+    check('role','No es un rol permitido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    validateFields
 ],usersPost);
 
 router.patch('/',usersPatch)
