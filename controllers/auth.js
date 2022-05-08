be0188492b81d7e0b694a1a1d64960d192c0a932
Poli03 <99154416+Poli03@users.noreply.github.com>
2022-05-08 16:45:28 -0500

    added confirm user exists

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,41 @@
+const bcryptjs = require('bcryptjs');
 const {response}= require('express');
+const User = require('../models/user');
 
-const login = (req ,res =response) =>{
-    res.json({
-        msg:'login OK'
-    });
+const login = async(req ,res =response) =>{
+    const {email,password}= req.body;
+
+    try {
+        //
+        const user = await User.findOne({email});
+        if (!user) {
+            return res.status(400).json({
+                msg:'Usuario / Contraseña no son correctos - correo'
+            });
+        }
+        
+        if (!user.status) {
+            return res.status(400).json({
+                msg:'Usuario / Contraseña no son correctos - estado: false'
+            });
+        }
+
+        const validPassword = bcryptjs.compareSync(password, user.password) ;
+        if (!validPassword) {
+            return res.status(400).json({
+                msg:'Usuario / Contraseña no son correctos - contraseña'
+            });
+        }
+
+        res.json({
+            msg:'login OK'
+        });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }
 }
 
 module.exports = {
