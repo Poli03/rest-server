ecb44115ca4c9770d72cb7bf36b2a02f7ec7a1f8
Poli03 <99154416+Poli03@users.noreply.github.com>
2022-05-08 16:22:23 -0500

    route auth added

---
+++ controllers/auth.js
@@ -0,0 +1,11 @@
+const {response}= require('express');
+
+const login = (req ,res =response) =>{
+    res.json({
+        msg:'login OK'
+    });
+}
+
+module.exports = {
+    login
+}
--- models/server.js
+++ models/server.js
@@ -6,6 +6,8 @@ class Server{
     constructor(){
         this.app = express();
         this.port= process.env.PORT;
+
+        this.aurhPath ='/api/auth';
         this.usersPath ='/api/users';
         
         //conecion a base de datos
@@ -31,6 +33,7 @@ class Server{
     }
 
     routes(){
+        this.app.use(this.aurhPath, require('../routes/auth'));
         this.app.use(this.usersPath, require('../routes/users'));
     }
 
